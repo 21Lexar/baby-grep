5682e7608d02be71f4d123bbba8de690acf4e21a
21Lexar <the-charon@hotmail.com>
2025-12-18 12:44:46 +0500

    Add search word in a line

--- src/search.c
+++ src/search.c
@@ -1,18 +1,25 @@
 #include<stdio.h>
+include<string.h>
+#include<ctype.h>
 #include "../include/search.h"
 
 void search(char* word_to_find, char* file_name){
 	FILE *file_ptr;
 	file_ptr = fopen(file_name, "r");
 	if(file_ptr == NULL){
-		printf("ERROR 255: Unable to open file\n");
+		printf("ERROR: Unable to open file\n");
 	}else{
 		char line;
+		bool status;
 		while(fgets(line, sizeof(line), file) != NULL){
-			
-			printf("&s", line);
+			if(strstr(line, word_to_find) != NULL){
+				prinf("&s",line);
+				status = true;
+			} 				
 		}
-
+		if(status == false){
+			printf("\n%s is not in the %s\n", word_to_find, file_name);
+		}
 	}
 	fclose(file_ptr);
 }
