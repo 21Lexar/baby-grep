f5435ed4c4b6dfd3521b5165860b4b4334376fe1
21Lexar <the-charon@hotmail.com>
2025-12-18 20:58:10 +0500

    Add line number func

--- src/search.c
+++ src/search.c
@@ -11,11 +11,13 @@ void search(char* word_to_find, char* file_name){
 	}else{
 		char line[1024];
 		int status = 0;
+		int line_number = 1;
 		while(fgets(line, sizeof(line), file_ptr) != NULL){
 			if(strstr(line, word_to_find) != NULL){
-				printf("%s",line);
+				printf("%d %s",line_number, line);
 				status = 1;
-			} 				
+			}
+			line_number = line_number + 1;
 		}
 		if(status == 0){
 			printf("\n%s is not in the %s\n", word_to_find, file_name);
