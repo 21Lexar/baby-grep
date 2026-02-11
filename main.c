376e4334889334a98e344d310d7ff7e774853e29
21Lexar <the-charon@hotmail.com>
2026-02-11 20:41:16 +0500

    optimize unoptimized code

--- main.c
+++ main.c
@@ -3,10 +3,11 @@
 #include "include/search.h"
 
 int main(int argc, char* argv[]){
-	if(argc == 3){
-		char* word_to_find = argv[1];
-		char* file_name = argv[2];
-		search(word_to_find, file_name);
+	if(argc != 3){
+		return -1;
 	}
+	char* word_to_find = argv[1];
+	char* file_name = argv[2];
+	search(word_to_find, file_name);
 	return 0;
 }
